32db70febe05a260e6ec8ccd7e70b77f36520f62
Nicolas Buduroi <nbuduroi@gmail.com>
2023-04-14 19:48:44 -0400

    Fix loading issue

--- init.el
+++ init.el
@@ -8,9 +8,30 @@
 ;;;;;;;; bash/readline configuration.
 ;;;;;;;;
 
+;;;; packages
+
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  ; initialize use-package on non-Linux platforms
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package load-relative)
+
 ;;;; functions
 
-(load-file "functions.el")
+(load-relative "functions.el")
 
 ;;;; vars
 
@@ -64,25 +85,6 @@
 (global-set-key "\C-z"    "()\C-b") ; displace suspend-frame
 (global-set-key "\M-z"    "[]\C-b") ; displace zap-to-char
 (global-set-key "\C-\M-z" "{}\C-b")
-
-;;;; packages
-
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  ; initialize use-package on non-Linux platforms
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
 
 ;;;; appearance
 
