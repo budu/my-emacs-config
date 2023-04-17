e58cea0e34221625a6e6f428ab15072fbbb99ff4
Nicolas Buduroi <nbuduroi@gmail.com>
2023-04-17 18:03:16 -0400

    Add key binding for counsel-apropos

--- init.el
+++ init.el
@@ -148,6 +148,7 @@
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
+         ("C-M-a" . counsel-apropos) ; displace beginning-of-defun
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history)))
 
