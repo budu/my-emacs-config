d16a3881d653e9631fb81dde922e301b1a1bb536
Nicolas Buduroi <nbuduroi@gmail.com>
2023-04-20 15:50:26 -0400

    Clean up

--- init.el
+++ init.el
@@ -108,12 +108,12 @@
 (global-set-key "\M-k"     'previous-window-any-frame)    ; displace kill-sentence
 (global-set-key "\C-\M-h"  'mark-paragraph)               ; displace mark-defun
 
-(global-set-key (kbd "C-'")      'mu/touch)
-(global-set-key (kbd "C-:")      "&:")
-(global-set-key (kbd "C-M-S-h")  'mark-defun)
-(global-set-key (kbd "<C-i>") "||\C-b") ; displace tab-bound function
-(global-set-key (kbd "<C-m>") 'mu/kmacro-start-or-end-macro) ; displace newline
-(global-set-key (kbd "<f12>") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "C-'")     'mu/touch)
+(global-set-key (kbd "C-:")     "&:")
+(global-set-key (kbd "C-M-S-h") 'mark-defun)
+(global-set-key (kbd "<C-i>")   "||\C-b") ; displace tab-bound function
+(global-set-key (kbd "<C-m>")   'mu/kmacro-start-or-end-macro) ; displace newline
+(global-set-key (kbd "<f12>")   (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
 
 (global-set-key "\C-z"    "()\C-b") ; displace suspend-frame
 (global-set-key "\M-z"    "[]\C-b") ; displace zap-to-char
