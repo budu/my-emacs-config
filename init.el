e81537ae57442e1e9d6c62f243602c42f6fb3248
Nicolas Buduroi <nbuduroi@gmail.com>
2024-01-01 16:05:56 -0500

    Tweak adoc-mode title heights

--- init.el
+++ init.el
@@ -429,7 +429,13 @@
 
 (use-package adoc-mode
   :mode "\\.adoc\\'"
-  :custom ((adoc-command "asciidoctor")))
+  :custom ((adoc-command "asciidoctor"))
+  :custom-face (adoc-title-0-face ((t (:height 1.5))))
+  :custom-face (adoc-title-1-face ((t (:height 1.3))))
+  :custom-face (adoc-title-2-face ((t (:height 1.2))))
+  :custom-face (adoc-title-3-face ((t (:height 1.1))))
+  :custom-face (adoc-title-4-face ((t (:height 1.0))))
+  :custom-face (adoc-title-5-face ((t (:height 0.9)))))
 
 ;;;; markdown
 
