fbe96fe38cb6957484ef5bd5a64e2d8edc293e72
Nicolas Buduroi <nbuduroi@gmail.com>
2024-01-15 12:35:25 -0500

    Add buttercup package and regroup elisp-related configuration

--- init.el
+++ init.el
@@ -345,7 +345,6 @@
 (use-package crystal-mode)
 (use-package dap-mode)
 (use-package dockerfile-mode)
-(use-package elisp-lint)
 (use-package geiser-racket)
 (use-package lua-mode)
 (use-package slim-mode)
@@ -377,6 +376,23 @@
 (use-package ein
   :bind (:map ein:notebook-mode-map
          ("<C-M-return>" . ein:worksheet-execute-cell)))
+
+;;;; elisp
+
+;; docs only show usage from command line:
+;; emacs -Q --batch -l elisp-lint.el -f elisp-lint-files-batch *.el
+;; https://github.com/gonewest818/elisp-lint
+(use-package elisp-lint)
+
+;; Usage from command line:
+;; emacs -batch -f package-initialize -L . -f buttercup-run-discover
+(use-package buttercup
+  :bind (:map buttercup-minor-mode-map
+         ("C-c C-t" . buttercup-run-at-point)))
+
+(use-package elisp-mode
+  :ensure nil
+  :hook ((emacs-lisp-mode . buttercup-minor-mode)))
 
 ;;;; restclient
 
