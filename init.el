199f77546ece9585683b90e0e0b0c4d9bf64194f
Nicolas Buduroi <nbuduroi@gmail.com>
2024-01-18 14:18:51 -0500

    Remove use-package initialization
    This is included in Emacs 29.

--- init.el
+++ init.el
@@ -23,11 +23,6 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(unless (package-installed-p 'use-package)
-  ; initialize use-package on non-Linux platforms
-  (package-install 'use-package))
-
-(require 'use-package)
 (setq use-package-always-ensure t)
 
 (use-package load-relative)
