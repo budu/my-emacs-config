b45e2971532d66b0badad74ed09c53237ada19d7
Nicolas Buduroi <nbuduroi@gmail.com>
2024-03-23 11:42:29 -0400

    Add back eglot configuration

--- init.el
+++ init.el
@@ -407,14 +407,13 @@
 
 ;;;; eglot
 
-;; (use-package eglot
-;;   :hook (prog-mode . eglot-ensure)
-;;   :init
-;;   (setq eldoc-echo-area-use-multiline-p nil
-;;         eglot-put-doc-in-help-buffer nil) ;'eglot-doc-too-large-for-echo-area)
-;;   :custom
-;;   (eglot-autoshutdown t)
-;;   (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp")))
+(use-package eglot
+  :init
+  (setq eldoc-echo-area-use-multiline-p nil
+        eglot-put-doc-in-help-buffer nil) ;'eglot-doc-too-large-for-echo-area)
+  :custom
+  (eglot-autoshutdown t)
+  (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp")))
 
 ;;;; copilot
 
