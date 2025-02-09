2d9410137de4017d0ef3e7a45080205b27388bd7
Nicolas Buduroi <nbuduroi@gmail.com>
2025-02-09 11:38:13 -0500

    Fix a few issues with create-ticket-todo

--- mu/cg.el
+++ mu/cg.el
@@ -8,19 +8,19 @@
   "Create a TODO entry for the specified TICKET-NUMBER."
   (interactive "nTicket number: ")
   (let ((today (format-time-string "%Y-%m-%d"))
-        (template-file (expand-file-name "nb-notes/templates/ticket.org")))
+        (template-file (expand-file-name "templates/ticket.org")))
     (if (file-exists-p template-file)
         (progn
           (find-file template-file)
           (let ((template-content (buffer-string)))
             (kill-buffer)
             (org-insert-heading)
-            (insert (format "TODO [#A] Ticket %d :ticket:\n" ticket-number))
+            (insert (format "TODO [#A] Ticket #%d :ticket:\n" ticket-number))
             (org-schedule nil today)
             (insert "\n")
             (insert template-content)
             (save-excursion
-              (while (search-forward "NUMBER" nil t)
+              (while (search-backward "NUMBER" nil t)
                 (replace-match (number-to-string ticket-number) t t)))))
       (message "Template file not found: %s" template-file))))
 
