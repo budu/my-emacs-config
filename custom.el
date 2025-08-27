85ac4eecd2b9f49f2334831f1e3fd01680033ef6
Nicolas Buduroi <nbuduroi@gmail.com>
2025-08-27 11:05:20 -0400

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,14 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/cg/reservotron/nb-notes/current.org"))
- '(package-selected-packages
-   '(magit poly-markdown rust-mode golden-ratio pkg-info flycheck origami copilot yari js2-mode mastodon rainbow-mode asdf rubocop robe buttercup adoc-mode screenshot editorconfig quelpa-use-package quelpa grip-mode restclient dockerfile-mode multiple-cursors sql-mode dap-mode crystal-mode crysal-mode rails-i18n ruby-end expand-region rspec-mode flyspell-correct-ivy flyspell-correct zenburn-theme yaml-mode which-key web-mode use-package tide svelte-mode smartparens slim-mode scss-mode rvm ruby-electric rjsx-mode rainbow-delimiters projectile-ripgrep projectile-rails lua-mode lsp-ui lsp-treemacs lsp-ivy load-relative json-mode ivy-rich helpful helm-rb helm-projectile helm-ispell haml-mode google-translate geiser-racket find-things-fast emojify emmet-mode elm-mode elixir-mix elisp-lint dumb-jump doom-themes doom-modeline direnv counsel-projectile company-web company-tabnine company-go company-box coffee-mode cl-libify autopair auto-dim-other-buffers all-the-icons alchemist))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el")))
  '(screenshot-font-size 8))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#121a1e"))))
  '(markdown-header-delimiter-face ((t (:foreground "hot pink" :inherit markdown-header-face))))
  '(markdown-header-face ((t (:foreground "hot pink" :inherit bold))))
  '(markdown-list-face ((t (:foreground "green"))))
