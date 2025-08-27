7a04a9be351e044838403c879d18a9eba162db1b
Nicolas Buduroi <nbuduroi@gmail.com>
2025-08-27 16:18:42 -0400

    Fix er/expand-region for namespaced module/class names

--- init.el
+++ init.el
@@ -268,7 +268,19 @@ Around advice for FUN with ARGS."
 ;;;; ivy
 
 (use-package expand-region
-  :bind (("M-h" . er/expand-region)))
+  :bind (("M-h" . er/expand-region))
+  :config
+  (defun er/add-ruby-mode-expansions ()
+    "Adds Ruby-specific expansions for buffers in ruby-mode"
+    (set (make-local-variable 'er/try-expand-list)
+         (remove 'er/mark-defun
+                 (append
+                  (default-value 'er/try-expand-list)
+                  '(er/mark-ruby-instance-variable
+                    er/c-mark-fully-qualified-name
+                    er/mark-ruby-block-up
+                    er/mark-ruby-heredoc)))))
+  (er/enable-mode-expansions 'ruby-mode #'er/add-ruby-mode-expansions))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
