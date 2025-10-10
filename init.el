9a680e683174cae4f82b95ecf6e7525f6adacfae
Nicolas Buduroi <nbuduroi@gmail.com>
2025-10-10 10:23:45 -0400

    Improve markdown styling

--- init.el
+++ init.el
@@ -708,7 +708,19 @@ Around advice for FUN with ARGS."
   (markdown-mode . markdown-toggle-markup-hiding)
   (markdown-mode . visual-line-mode)
   :config
-  (setq markdown-command "~/bin/pandoc"))
+  (setq markdown-command "~/bin/pandoc")
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(markdown-header-face-1 ((t (:inherit markdown-header-face :foreground "yellow" :height 1.0))))
+   '(markdown-header-face-2 ((t (:inherit markdown-header-face :foreground "purple" :height 1.0))))
+   '(markdown-header-face-3 ((t (:inherit markdown-header-face :foreground "magenta" :height 1.0))))
+   '(markdown-header-face-4 ((t (:inherit markdown-header-face :foreground "dark cyan" :height 1.0)))))
+)
+
+
 
 ;;;; Transient
 
